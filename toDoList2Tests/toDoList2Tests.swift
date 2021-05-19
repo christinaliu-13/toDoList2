d82b29d41b20bb8a20c3bf9a99e55846dea44749
Christina Liu <christinaflorida@gmail.com>
2021-05-18 20:46:20 -0700

    Initial Commit

---
+++ toDoList2Tests/toDoList2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  toDoList2Tests.swift
+//  toDoList2Tests
+//
+//  Created by Christina Liu on 5/18/21.
+//  Copyright © 2021 Christina Liu. All rights reserved.
+//
+
+import XCTest
+@testable import toDoList2
+
+class toDoList2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

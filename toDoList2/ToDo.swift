65e035341d1544a39f04e310890bcf5c7a93f2dc
Christina Liu <christinaflorida@gmail.com>
2021-05-18 21:35:55 -0700

    Attempt on Iteration 0

---
+++ toDoList2/ToDo.swift
@@ -0,0 +1,18 @@
+//
+//  ToDo.swift
+//  toDoList2
+//
+//  Created by Christina Liu on 5/18/21.
+//  Copyright © 2021 Christina Liu. All rights reserved.
+//
+
+import UIKit
+
+class ToDo
+{
+    var name = "";
+    var important = false;
+    //we can't declare important as a bool, so we do var instead
+}
+
+
